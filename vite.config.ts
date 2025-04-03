6ff5b44d185b505ca8c7cb15b9c4b3754bb5d810
Josué Hernández <joheca1998@outlook.com>
2025-04-03 17:17:03 -0600

    Configuring the environment to apply tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  test: {
+    environment: 'jsdom', // Simular el navegador
+    globals: true, // Permitir uso global de funciones de test
+    setupFiles: ['./src/setuptest.ts'], // Configuración global previa a cada test
+  },
   plugins: [react()],
 })
